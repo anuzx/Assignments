3ac633597bce62958e1a0a62592d190edfd385d8
anuzx <anujparasar4266@gmail.com>
2026-02-16 23:51:30 +0530

    hotel db

--- web-dev/Advanced-Javascript-Assignments/promises/easy/promiseAll.js
+++ web-dev/Advanced-Javascript-Assignments/promises/easy/promiseAll.js
@@ -4,7 +4,7 @@
 // The function should accept an array of values that may include Promises or plain constants. 
 // It must resolve with an array of results in the same order once all inputs resolve, or reject immediately if any input rejects.
 function promiseAll(promises) {
-    
+   
 }
 
 module.exports = promiseAll;
