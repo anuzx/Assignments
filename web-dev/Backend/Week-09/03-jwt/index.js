2930df226e13e2ac6c56e0d68f4f55a5ce9eb346
anuzx <anujparasar4266@gmail.com>
2026-04-13 16:10:42 +0530

    week 09 done

---
+++ web-dev/Backend/Week-09/03-jwt/index.js
@@ -0,0 +1,39 @@
+const jwt = require("jsonwebtoken")
+const jwtPassword = "secret_key"
+
+/**
+ * Generates a "Short-Lived" JWT that expires in 1 minute.
+ * * @param {string} username - The user's email.
+ * @returns {string} A JWT that will be invalid after 60 seconds.
+ */
+function signShortLivedToken(username) {
+
+  return jwt.sign({ username }, jwtPassword, { expiresIn: "1m" })
+}
+
+/**
+ * Checks if a token is still valid or has expired.
+ * * @param {string} token - The JWT string.
+ * @returns {string} Returns "valid", "expired", or "invalid".
+ */
+function checkTokenStatus(token) {
+  // Your code here
+
+  try {
+
+    jwt.verify(token, jwtPassword)
+
+    return "valid"
+  } catch (error) {
+    if (error instanceof jwt.TokenExpiredError) {
+      return "expired";
+    }
+    return "invalid"
+  }
+}
+
+module.exports = {
+  signShortLivedToken,
+  checkTokenStatus,
+  jwtPassword
+}
